a4d953522694d76bbe7b55e1e99724e8cd8c8b5b
Joseph Murray <70991796+asparageist@users.noreply.github.com>
2023-11-20 20:50:15 -0800

    build client from lesson

---
+++ ShelterClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace ShelterClient.Models;
+
+#nullable enable
+
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
